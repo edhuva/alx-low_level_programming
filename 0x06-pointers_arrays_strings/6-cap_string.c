9d530f97cc0d9bb7a420ad73117d82ca833d913a
edhuva <edhuva491@gmail.com>
2023-03-30 16:42:25 -0700

    fix error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,6 +6,7 @@
  *
  * Return: A pointer to the changed string.
  */
+
 char *cap_string(char *str)
 {
 	int index = 0;
@@ -32,5 +33,6 @@ char *cap_string(char *str)
 		}
 		index++;
 	}
+
 	return (str);
 }
