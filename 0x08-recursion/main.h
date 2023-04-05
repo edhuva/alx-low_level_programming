04012ff1ab1572a8338d24e3001673888a412b9a
edhuva <edhuva491@gmail.com>
2023-04-05 14:57:16 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int act_sqrt_recursion(int n, int i);
+int is_prime_number(int n);
+int act_prime(int n, int j);
+int is_palindrome(char *s);
+int chek_pal(char *s, int j, int len);
+int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
