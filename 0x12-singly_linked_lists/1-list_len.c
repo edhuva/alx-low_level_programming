c8aad820999d19246760082f65df2c48cc4e0dc1
edhuva <edhuva491@gmail.com>
2023-04-28 14:30:28 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "lnklists.h"
+#include "lists.h"
 
 /**
  * list_len - returns the number of elements
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "lnklists.h"
+#include "lists.h"
 
 /**
  * free_list - frees a linked list
