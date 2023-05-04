a85ca138d8b8692c22e527daf86e31ac74d7d3bf
edhuva <edhuva491@gmail.com>
2023-05-03 18:18:07 -0700

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	neww = malloc(sizeof(listint_t));
 	if (!neww)
 		return (NULL);
-	
+
 	neww->n = n;
 	neww->next = NULL;
 	if (*head == NULL)
